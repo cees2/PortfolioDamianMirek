632440f7d0730736bdd72390394348c192791403
cees2 <damianmirek1234@gmail.com>
2022-08-23 12:50:54 +0200

    quiz beta v 1.0.1

--- src/components/Quiz/StartQuiz.js
+++ src/components/Quiz/StartQuiz.js
@@ -17,6 +17,7 @@ const StartQuiz = () => {
   const difficultyInputRef = useRef();
 
   useEffect(() => {
+    quizCtx.resetQuizData();
     const getData = async function () {
       const response = await fetch(
         `https://opentdb.com/api.php?amount=10&category=18&difficulty=${difficulty}`
@@ -50,7 +51,7 @@ const StartQuiz = () => {
         <div className={classes.quizImageAndDescription}>
           <p className={classes.quizDescription}>
             Computer science is a fast-developing part of industry. Imagine our
-            world Without this kind of technology - it would be a totally
+            world without this kind of technology - it would be a totally
             different place. It makes our lives simpler and better. Deep
             undestanding of just a branch of computer science is highly valuable
             nowadays. Check out your knowledge with the following quiz.

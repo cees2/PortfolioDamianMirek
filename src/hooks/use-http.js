f2e0cdf7df6fc740f4a26e586a7fef3c2cddfd46
cees2 <damianmirek1234@gmail.com>
2022-10-05 20:23:08 +0200

    Changed requests endpoint

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 
-export const DOMAIN = `http://127.0.0.1:3000/api/v1`;
+export const DOMAIN = `https://portfoliobackenddamian.herokuapp.com/api/v1`;
 
 const useHttp = () => {
   const sendRequest = useCallback(async (fetchOptions) => {
